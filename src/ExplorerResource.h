1f760a49b1c60737d3a2089fc59f761c60f4e05c
ZhuXuefengGitHub <136561134+ZhuXuefengGitHub@users.noreply.github.com>
2024-07-12 16:37:59 +0800

    Add files via upload

---
+++ src/ExplorerResource.h
@@ -0,0 +1,13 @@
+#ifndef EXPLORER_RC_H
+#define EXPLORER_RC_H
+
+#define IDI_EXPLORE							111
+
+#define		IDD_EXPLORER_DLG  30500
+
+#define	   EXX_TIMERS		 30820
+#define EXT_UPDATEACTIVATE				(EXX_TIMERS + 3)
+
+#define	   EXX_TIMERS		 30820
+#define	EXT_UPDATEACTIVATEPATH			(EXX_TIMERS + 4)
+#endif // EXPLORER_RC_H
